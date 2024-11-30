e57215278e76f8e5b2ae8a44bf622480b8e56f5e
Niqueeeee <103115381+niquehandi@users.noreply.github.com>
2024-11-30 13:40:00 +0700

    Update GraphBuilder.java

--- src/main/java/GraphBuilder.java
+++ src/main/java/GraphBuilder.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class GraphBuilder {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class GraphBuilder {
+    Map<Integer, Set<Integer>> graph;
+
+    private GraphBuilder() {
+        graph = new HashMap<>();
+    }
+
+    public static GraphBuilder emptyGraph() {
+        return new GraphBuilder();
+    }
+
+    public GraphBuilder edge(int u, int v) {
+        if (!graph.containsKey(u)) graph.put(u, new HashSet<>());
+        graph.get(u).add(v);
+        return this;
+    }
+
+    public Map<Integer, Set<Integer>> build() {
+        return graph;
+    }
 }
